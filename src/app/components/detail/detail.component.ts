348969ea2cedfd0d566f20601151d269fd7f5449
baguskurnia <bagus.kurnia@rga.com>
2022-01-15 19:40:32 +0700

    Add detail pokemon

--- src/app/components/detail/detail.component.ts
+++ src/app/components/detail/detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { ApiService } from 'src/app/services/api.service';
 
 @Component({
@@ -7,13 +8,19 @@ import { ApiService } from 'src/app/services/api.service';
   styleUrls: ['./detail.component.scss']
 })
 export class DetailComponent implements OnInit {
+  id: string = '';
+  name: string = '';
+  picture: string = '';
 
-  constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.apiService.getPokemonDetail(1).subscribe((data) => {
+    this.id = this.route.snapshot.paramMap.get('id') ?? '1';
+    this.apiService.getPokemonDetail(+this.id).subscribe((data) => {
       console.log(data);
-    })
+      this.name = data.name;
+      this.picture = data.sprites.front_default;
+    });
   }
 
 }
