108594737545c7152fab9c83d06b8aec6a9a4544
Bagus Kurnia <baguskna@bagus-macbook-pro.openvpn>
2022-01-16 19:13:40 +0700

    Add capacitor and modify article layout

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.pokedex.app',
+  appName: 'pokedex',
+  webDir: 'dist/pokedex',
+  bundledWebRuntime: false
+};
+
+export default config;
